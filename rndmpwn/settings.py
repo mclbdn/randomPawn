d7b164e56a9e817bee8bac6884b315cbf24c5e47
MichalBednar <michalbednar93@gmail.com>
2021-01-19 15:26:32 -0600

    something something

--- rndmpwn/settings.py
+++ rndmpwn/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mq0p+4o)on7#ureq=ab544_2#xld7gmr^p+a76)ko2*1$5et3q'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
