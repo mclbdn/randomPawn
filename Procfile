6e1a23213c5c48d1be07274654a310783ca00b61
MichalBednar <michalbednar93@gmail.com>
2021-01-18 23:25:03 -0600

    make it better

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn rndmpwn.wsgi --log-file -
---
+++ website/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+import requests
+import json
+from random import randint
+import re
+
+# Create your views here.
+def website(request):
+    r = requests.get('https://haveibeenpwned.com/api/v3/breaches')
+
+    data = r.json()
+
+    lst_length = len(data)
+
+    random_breach = randint(0, lst_length)
+
+    title = (data[random_breach]['Title'])
+    breachDate = (data[random_breach]['BreachDate'])
+    domain = (data[random_breach]['Domain'])
+    description = (data[random_breach]['Description'])
+    description = re.sub('<a.*?>|</a>|<em>|</em>|&quot;', '', description)
+    data_compromised = data[random_breach]['DataClasses']
+
+    context = {
+        'title': title,
+        'breachDate': breachDate,
+        'domain': domain,
+        'description': description,
+        'data_compromised': data_compromised
+    }
+
+    return render(request, 'base.html', context)
